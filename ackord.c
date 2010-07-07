cfc51f9db32abbe65061df0ae516d36f289f910e
Sebastian <sebastian@liem.se>
2010-07-07 23:07:21 +0200

    added some debounce logic - which don't work at all

--- ackord.c
+++ ackord.c
@@ -1,3 +1,4 @@
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
@@ -8,7 +9,13 @@
 
 int
 main(void) {
+  uint8_t t;
+
   CPU_PRESCALE(0); /* 16 MHz */
+
+  /* configure the pins */
+  DDRB  = 0x00;
+  PORTB = 0x00;
 
   usb_init();
 
@@ -17,31 +24,55 @@ main(void) {
   _delay_ms(1000);
 
   usb_keyboard_press(KEY_A, 0);
-
+  
+  /* timer0 to overflow every 256*1024 clock cycles */
+  TCCR0A = 0x00;
+  TCCR0B = 0x05;
+  TIMSK0 = (1<<TOIE0);
+  sei();
+ 
+  
   while(1) {
+    t = readpins_debounced();
+
+    if (t != 0) {
+      usb_keyboard_press(pgm_read_byte_near(keymap + t), 0);
+    }
   }
 }
 
 uint8_t
 readpins(void) {
-  uint8_t b, d, o;
+  return PINB;
+}
 
-  b = PINB; d = PIND; o = 0;
-
-  o |= b & (1<<0); /* B0 */
-  o |= b & (1<<1); /* B1 */
-  o |= b & (1<<2); /* B2 */
-  o |= b & (1<<3); /* B3 */
-  o |= b & (1<<4); /* B7 */
-
-  if (d & (1<<0))  /* D0 */
-    o |= (1<<5);
-
-  if (d & (1<<1))  /* D1 */
-    o |= (1<<6);
-
-  if (d & (1<<2))  /* D2 */
-    o |= (1<<7);
+uint8_t
+readpins_debounced(void) {
+  uint8_t o, sreg;
+  
+  sreg = SREG;
+  cli();
+  o = btnstate;
+  SREG = sreg;
 
   return o;
+}
+
+ISR(TIMER0_OVF_vect) {
+  static volatile uint8_t oldstate, newstate, count;
+
+  newstate = readpins();
+
+  if (newstate == oldstate) { 
+    count++;
+  } else {
+    count = 0;
+  }
+
+  oldstate = newstate;
+
+  if (count > 10) {
+    btnstate = newstate;
+    count = 0;
+  }
 }
